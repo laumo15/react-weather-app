72ebe0b2c144edd19f187f5d10fb4ceda175bea6
laumo15 <118842475+laumo15@users.noreply.github.com>
2023-03-27 17:43:05 +0200

    Edited footer

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Weather />
-        <a
-          className="App-link"
-          href="https://github.com/laumo15/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-sourced on GitHub
-        </a>
+        <footer>
+          This project was coded by{" "}
+          <a
+            className="App-link"
+            href="https://portfolio-building-project.netlify.app/work.html" target="_blank" rel="noreferrer"
+          >
+            Laura Morelli
+          </a>{" "}
+          and is open sourced on {" "}
+          <a
+            className="App-link"
+            href="https://github.com/laumo15/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
       </header>
     </div>
   )
