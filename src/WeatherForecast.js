ef95309a609cf38491e1bc4fb0e3646fef7b3110
laumo15 <118842475+laumo15@users.noreply.github.com>
2023-04-09 23:40:56 +0200

    Added forecast for days 2 to 5

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -10,10 +10,16 @@ export default function WeatherForecast(props) {
     if (loaded) {
         return (
           <div className="WeatherForecast">
-            <div className="row">
-              <div className="col">
-                    <WeatherForecastDay data={forecast[0]} />
-              </div>
+                <div className="row">
+                    {forecast.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                          return (
+                            <div className="col" key="index">
+                              <WeatherForecastDay data={dailyForecast} />
+                            </div>
+                          )
+                        };
+                    })}
             </div>
           </div>
         )
